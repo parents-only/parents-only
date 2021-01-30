4cf7f03137e5e5d69eb1ee597296f63ffa8f7e2c
Murphy <sydneyporter74@yahoo.com>
2021-01-30 15:33:24 -0500

    added call-to-action form

--- client/src/components/LandingPage/index.js
+++ client/src/components/LandingPage/index.js
@@ -3,7 +3,7 @@ import { photos, photos2 } from './photos';
 
 import { render } from 'react-dom';
 import Gallery from 'react-photo-gallery';
-import {  Card } from 'react-bootstrap';;
+import {  Card, Button } from 'react-bootstrap';;
 
 const LandingPage = () => {
 
@@ -11,9 +11,8 @@ const LandingPage = () => {
         <div>
             <div>
                 <Gallery photos={photos} />
-            </div>
-            <div>            
-                <Card style={{ width: '18rem', textAlign: "center", margin: "0 auto", marginBottom: 10 }}>
+            
+                <Card style={{ width: '18rem', textAlign: "center", margin: "0 auto", marginBottom: 10, position: "absolute", left: "calc( 50% - 144px )", backgroundColor: "rgb( 255, 255, 255, 0.8 )" }}>
                     <Card.Body>
                         <Card.Title>Parents Only</Card.Title>
                         <Card.Subtitle className="mb-2 text-muted">Join us today!</Card.Subtitle>
@@ -24,10 +23,22 @@ const LandingPage = () => {
                         <Card.Link href="">Sign up</Card.Link>
                     </Card.Body>
                 </Card>
-            </div>
-            <div>
+            
+            
                 <Gallery photos={photos2} />
             </div>
+            <div className="container">
+                <div className="col-md-12 col-xs-12 col-sm-12" style={{ textAlign: "center" }}>
+                    <div className="display-4" style={{ marginTop: "1em" }}>FIND YOUR NEW BEST FRIENDS TODAY</div>
+                    <div style={{ fontSize: "20px", marginBottom: "1em"}}>Looking for friends with children? Do you have children? Start looking for your new best friends today!</div>
+                    <div className="mb-2" style={{ marginBottom: "2em" }}>
+                        <Button variant="primary" size="lg" style={{ margin: "1em" }}>Join Free</Button>
+                        <Button variant="primary" size="lg">Contact us</Button>
+                    </div>
+                    
+                </div>
+            </div>
+            
         </div>
     )
     
