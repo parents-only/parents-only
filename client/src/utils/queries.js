ec2c88ab53072486e184fa2c0e7b2535356d8d5b
Murphy <sydneyporter74@yahoo.com>
2021-02-02 17:34:55 -0500

    added query-users

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -57,6 +57,16 @@ export const QUERY_USER = gql`
   }
 `;
 
+export const QUERY_USERS = gql `
+    query users {
+        users {
+            _id
+            username
+            email
+        }
+    }
+`;
+
 export const QUERY_ME = gql`
   {
     me {
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -6,6 +6,7 @@ const typeDefs = gql`
     username: String
     email: String
     password: String
+    image: String
     friendCount: Int
     messages: [Message]
     friends: [User]
