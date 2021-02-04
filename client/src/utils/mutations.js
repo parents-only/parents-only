4c298bfd7755108d74872e959b8678f72f46572e
QuaggWasTaken <forrestfaulkner1@gmail.com>
2021-02-03 22:48:20 -0600

    Partial User editing and upload avatar

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -24,6 +24,14 @@ export const ADD_USER = gql`
   }
 `;
 
+export const UPDATE_USER = gql`
+  mutation updateUser($username: String, $email: String, $age: Int, $bio: String ) {
+    updateUser(username: $username, email: $email, age: $age, bio: $bio) {
+        _id
+    }
+  }
+`;
+
 export const ADD_MESSAGE = gql`
   mutation addMessage($messageText: String!) {
     addMessage(messageText: $messageText) {
