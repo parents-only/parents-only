1ea6cca5fd6f6c1b5579d7f2bed178e4e5da628d
Murphy <sydneyporter74@yahoo.com>
2021-02-06 13:31:18 -0500

    added new photo

--- client/src/components/Carousel/index.js
+++ client/src/components/Carousel/index.js
@@ -27,7 +27,7 @@ const PictureSlider = () => {
             <Carousel.Item>
                 <img
                 className="d-block w-100"
-                src="/img/landingpage3.jpg"
+                src="/img/landingpage.jpg"
                 alt="Third slide"
                 style={{ width: "6000", height: "4000" }}
                 />
