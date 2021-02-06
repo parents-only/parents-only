b523ba84743e6738a3f253b98991510e19470ddc
Mera Youssef <merayoussef91@hotmail.com>
2021-02-05 19:44:40 -0600

    added username to switch cards

--- client/src/components/SwitchCard/examples/Simple.js
+++ client/src/components/SwitchCard/examples/Simple.js
@@ -49,7 +49,7 @@ function Simple() {
                 {characters.map((character) =>
                     <TinderCard className='swipe' key={character._id} onSwipe={(dir) => swiped(dir, character._id)} onCardLeftScreen={() => outOfFrame(character._id)}>
                         <div style={{ backgroundImage: 'url(' + character.avatar + ')' }} className='card'>
-                            <h3>{character.name}</h3>
+                            <h3>{character.username}</h3>
                         </div>
                     </TinderCard>
                 )}
